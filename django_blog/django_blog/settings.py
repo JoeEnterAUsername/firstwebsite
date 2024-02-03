5ef73baddc95e05be187df4ed016de916f4908c2
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-02-03 00:16:23 +0000

    Minor changes while learning

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "users.apps.UsersConfig",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "blog.apps.BlogConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -126,3 +128,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'BOOTSTRAP5'
