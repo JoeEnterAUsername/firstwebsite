db74f27fa4247cc3edfe035280b7413bdd775c25
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-02-14 22:14:01 +0000

    minor changes

--- django_blog/users/views.py
+++ django_blog/users/views.py
@@ -9,7 +9,10 @@ def register(request):
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Account created for {username}")
-            return redirect("blog-home")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render (request, "users/register.html", {"form": form})
+
+def profile(request):
+    return render(request, "users/profiles.html")
